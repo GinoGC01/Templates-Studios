87978589020c1ac1fbcc677406a43e31e98fdc05
GinoDesktop <ginociancia10@gmail.com>
2024-06-05 22:12:16 -0300

    template-01 V0

---
+++ template-01/src/components/React/header.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import "./header.css";
+export default function header() {
+  const [on, setOn] = useState(false);
+  const handleNav = () => {
+    setOn(!on);
+  };
+
+  return (
+    <header className="header-principal">
+      <div className="header-principal_logo">
+        <img src="../../../../images/temp/logo-temp-01.svg" alt="logo-temp" />
+        <div className="">
+          <strong>Estudio Jurídico</strong>
+          <p>Nombre estudio</p>
+        </div>
+      </div>
+      <span className="menu-hamburguesa" onClick={handleNav}>
+        <span
+          className={
+            on ? "menu-hamburguesa_span child-1 " : "menu-hamburguesa_span"
+          }
+        ></span>
+        <span
+          className={
+            on ? "menu-hamburguesa_span child-2 " : "menu-hamburguesa_span"
+          }
+        ></span>
+        <span
+          className={
+            on ? "menu-hamburguesa_span child-3 " : "menu-hamburguesa_span"
+          }
+        ></span>
+      </span>
+      <nav
+        className={
+          on
+            ? "header-principal_nav header-principal_nav-on"
+            : "header-principal_nav"
+        }
+      >
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#Sobre-nosotros">Sobre Nosotros</a>
+          </li>
+          <li>
+            <a href="#Noticias">Noticias</a>
+          </li>
+          <li>
+            <a href="#">Testimonios</a>
+          </li>
+          <li>
+            <a href="#">Contacto</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
